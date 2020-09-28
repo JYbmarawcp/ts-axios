88ee9f02931aaa570fcd7ffd86ad285fef9ed20c
JYbmarawcp <fjh2333@gmail.com>
2020-09-28 20:47:34 +0800

    feat(配置serve端完成):

---
+++ src/types/index.ts
@@ -0,0 +1,24 @@
+import { type } from 'os'
+
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'DEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
