6395263cfb6cb2080d7b3e63c439b84494db3be0
JYbmarawcp <fjh2333@gmail.com>
2020-09-29 15:23:12 +0800

    feat(处理请求header): 包括Content-Type的值处理

---
+++ src/helpers/headers.ts
@@ -0,0 +1,26 @@
+import { isPlainObject } from './utils'
+
+// header 属性是大小写不敏感的,例子中传入content-type需要规范化
+function normalizeHeaderName(headers: any, normalizedName: string): void {
+  if (!headers) {
+    return
+  }
+  Object.keys(headers).forEach(name => {
+    if (name !== normalizedName && name.toUpperCase() === normalizedName.toUpperCase()) {
+      headers[normalizedName] = headers[name]
+      delete headers[name]
+    }
+  })
+}
+
+export function processHeaders(headers: any, data?: any): any {
+  normalizeHeaderName(headers, 'Content-Type')
+
+  if (isPlainObject(data)) {
+    if (headers && !headers['Content-Type']) {
+      headers['Content-Type'] = 'application/json;charset=UTF-8'
+    }
+  }
+
+  return headers
+}
--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -12,5 +12,5 @@ export function isObject(val: any): val is Object {
 // 如果是FormData typeof为object, 但toString.call为[object FormData]
 // 所以需要一个判断普通对象的函数
 export function isPlainObject(val: any): val is Object {
-  return toString.call(val) === '[object object]'
+  return toString.call(val) === '[object Object]'
 }
