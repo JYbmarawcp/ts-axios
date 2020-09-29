1c45ff1d1359166391eecaffe4703fb239d7bfaf
JYbmarawcp <fjh2333@gmail.com>
2020-09-29 20:48:12 +0800

    feat(错误处理): 处理网络异常错误;超时错误;非200状态码错误

---
+++ examples/error/app.ts
@@ -0,0 +1,44 @@
+import axios from '../../src/index'
+
+// 404
+axios({
+  method: 'get',
+  url: '/error/get1'
+}).then((res) => {
+  console.log(res)
+}).catch((e) => {
+  console.log(e)
+})
+
+// 500
+axios({
+  method: 'get',
+  url: '/error/get'
+}).then((res) => {
+  console.log(res)
+}).catch((e) => {
+  console.log(e)
+})
+
+// 模拟网络错误
+setTimeout(() => {
+  axios({
+    method: 'get',
+    url: '/error/get'
+  }).then((res) => {
+    console.log(res)
+  }).catch((e) => {
+    console.log(e)
+  })
+}, 5000)
+
+// 超时
+axios({
+  method: 'get',
+  url: '/error/timeout',
+  timeout: 2000
+}).then((res) => {
+  console.log(res)
+}).catch((e) => {
+  console.log(e.message);
+})
