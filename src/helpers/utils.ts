9ee6187057b4cef9d83d1492682ac9eb67071b08
JYbmarawcp <fjh2333@gmail.com>
2020-09-29 14:38:53 +0800

    feat(处理data): 普通对象转化为JSON字符串

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -8,3 +8,9 @@ export function isDate(val: any): val is Date {
 export function isObject(val: any): val is Object {
   return val !== null && typeof val === 'object'
 }
+
+// 如果是FormData typeof为object, 但toString.call为[object FormData]
+// 所以需要一个判断普通对象的函数
+export function isPlainObject(val: any): val is Object {
+  return toString.call(val) === '[object object]'
+}
